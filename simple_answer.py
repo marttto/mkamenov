6f969b42fd813c4f997a3e40f1a6ef4d5dd84407
marttto <31631283+marttto@users.noreply.github.com>
2017-09-05 10:00:19 +0300

    Create simple_answer.py

---
+++ simple_answer.py
@@ -0,0 +1,16 @@
+# simple_answer.py
+
+text = input("Enter your text here: ")
+text = str(text)
+
+if text == "hello" or text =="Hello":
+    print("Hello there, good stranger!")
+elif text == "how are you?":
+    print("I am fine, thanks. How are you?")
+elif text == "feelings":
+    print("I am a machine. I have no feelings")
+elif text == "age":
+    print("I have no age. Only current timestamp")
+
+# Ако в текста се среща повече от 1 от думите, за които гледаме, отговорете с това, с което първо проверявате.
+# Само това условия не се сещам как да го направя?!?
