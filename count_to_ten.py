78dbd01d8d9dde3e72849f40363d325691b178cc
marttto <31631283+marttto@users.noreply.github.com>
2017-09-05 13:28:50 +0300

    Create count_to_ten.py

---
+++ count_to_ten.py
@@ -0,0 +1,13 @@
+# count_to_ten.py
+
+counter1 = 0
+
+while counter1 <= 10:
+    print(counter1)
+    counter1 = counter1 + 1
+
+counter2 = 10
+
+while counter2 >= 0:
+    print(counter2)
+    counter2 = counter2 - 1
